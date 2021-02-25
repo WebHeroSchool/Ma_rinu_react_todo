97a5c4b55916ea8ab6d47d567d4729082e036bbe
Marina Mikhailova <mmmarinammm@gmail.com>
2021-02-25 14:26:28 +0300

    урок 23

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import styles from'./Footer.module.css'
 
-const Footer = ({ count }) => (<div>Осталось выполнить вот столько дел: {count}</div>);
+const Footer = ({ count }) => (<div className={styles.text}>Осталось выполнить вот столько дел: {count}</div>);
 
 export default Footer;
