15494e0c1a62e850e23236016b1fa279932ca895
Marina Mikhailova <mmmarinammm@gmail.com>
2021-03-02 18:36:48 +0300

    урок 27

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,28 +6,31 @@ import styles from'./App.module.css';
 
 const todoItem = 'Написать новое приложение';
 
-const App = () => {
-  const items = [
-    {
-        value: 'Написать новое приложение',
-        isDone: true
-    },
-    {
-        value: 'Прописать props',
-        isDone: false
-     },
-    {
-        value: 'Сделать все дела',
-        isDone: true
-    }
-  ];
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+          value: 'Написать новое приложение',
+          isDone: true
+      },
+      {
+          value: 'Прописать props',
+          isDone: false
+       },
+      {
+          value: 'Сделать все дела',
+          isDone: true
+      }
+    ];
 
-  return (
-    <div className={styles.wrap}>
-      <h1 className={styles.title}>Важные дела:</h1>
-      <InputItem />
-      <ItemList items={items} />
-      <Footer count={3} />
-    </div>);}
+    return (
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>Важные дела:</h1>
+        <InputItem />
+        <ItemList items={items} />
+        <Footer count={3} />
+      </div>);
+  }
+};
 
 export default App;
