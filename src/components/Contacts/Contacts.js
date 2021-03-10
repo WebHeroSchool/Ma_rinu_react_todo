ed058440a672646d50719ad304d0a58b1ab431dc
Marina Mikhailova <mmmarinammm@gmail.com>
2021-03-10 21:23:11 +0300

    урок 39

---
+++ src/components/Contacts/Contacts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Contacts.module.css';
+
+const Contacts = () => {
+	return (
+		<div className={styles.wrap}>
+			<h1 className={styles.title}>Контакты</h1>
+		</div>
+	);
+}
+
+export default Contacts;
